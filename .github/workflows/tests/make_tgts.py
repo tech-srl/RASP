0f43d55a21b344a4575315445a3a4e5002b87318
Gail Weiss <gail_weiss@msn.com>
2021-07-26 17:32:40 +0300

    fix paths in error prints, make them match what github tester path will be. remove broken_rasplib/out (only want broken_rasplib/tgt)

--- .github/workflows/tests/make_tgts.py
+++ .github/workflows/tests/make_tgts.py
@@ -5,7 +5,7 @@ import os
 def things_in_path(path):
 	if not os.path.exists(path):
 		return []
-	return os.listdir(path)
+	return [p for p in os.listdir(path) if not p==".DS_Store"]
 
 testpath = ".github/workflows/tests"
 inpath = testpath+"/in"
@@ -16,8 +16,23 @@ if not os.path.exists(tgtpath):
 
 all_names = things_in_path(inpath)
 
+def fix_file_paths(filename):
+	mypath  = os.path.abspath(".")
+	gitpath = '/home/runner/work/RASP/RASP' # will probably change over time oh well
+
+	mypath = mypath.replace("/","\/")
+	gitpath = gitpath.replace("/","\/")
+
+
+	cmd = "sed -i '.txt' 's/"+mypath+"/"+gitpath+"/' "+filename
+	print(cmd)
+	os.system(cmd)
+	os.system("rm "+filename+".txt")
+
+
 def run_input(name):
 	os.system("python3 RASP_support/REPL.py <"+inpath+"/"+name+" >"+tgtpath+"/"+name)
+	fix_file_paths(tgtpath+"/"+name)
 
 def run_inputs():
 	for n in all_names:
