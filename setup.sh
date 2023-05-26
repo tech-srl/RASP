ae5950e68bc49204c56c3f5ae60e43647ec06987
Emile Ferreira <32413750+emileferreira@users.noreply.github.com>
2023-05-26 12:57:54 +0200

    Add other Linux package managers to setup script (#9)

--- setup.sh
+++ setup.sh
@@ -14,8 +14,19 @@ if [ "$(uname)" == "Darwin" ]; then
 		HOMEBREW_NO_AUTO_UPDATE=1 brew install rlwrap
 	fi
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	sudo apt-get install graphviz
-	sudo apt-get install rlwrap
+	if which apt-get > /dev/null 2>&1; then
+		sudo apt-get install graphviz rlwrap
+	elif which pacman > /dev/null 2>&1; then
+		sudo pacman -S --needed graphviz rlwrap
+	elif which dnf > /dev/null 2>&1; then
+		sudo dnf install graphviz rlwrap
+	elif which emerge > /dev/null 2>&1; then
+		sudo emerge --ask media-gfx/graphviz app-misc/rlwrap
+	else
+		echo "Attempted to install graphviz and rlwrap, but your package manager is not supported by this script. Consider installing them manually:"
+		echo $rlwrap_desc
+		echo $graphviz_desc
+	fi
 else
 	echo "For the best experience, you should install graphviz and rlwrap."
 	echo "I'm sorry, I don't know how to do this on your OS (please email me with OS+directions if you do, so I can update!)"
