309fb81e873c4de09cae62f3f611c4bec1bc6dfe
Emile Ferreira <32413750+emileferreira@users.noreply.github.com>
2023-07-15 15:59:50 +0200

    Format Python according to PEP 8

--- tests/make_tgts.py
+++ tests/make_tgts.py
@@ -15,71 +15,81 @@ liboutspath = libtestspath+"/out"
 curr_path_marker = "[current]"
 
 
-REPL_path = "RASP_support/REPL.py"
-rasplib_path = "RASP_support/rasplib.rasp"
+REPL_PATH = "RASP_support/REPL.py"
+RASPLIB_PATH = "RASP_support/rasplib.rasp"
+
 
 def things_in_path(path):
-	if not os.path.exists(path):
-		return []
-	return [p for p in os.listdir(path) if not p==".DS_Store"]
+    if not os.path.exists(path):
+        return []
+    return [p for p in os.listdir(path) if not p == ".DS_Store"]
+
 
 def joinpath(*a):
-	return "/".join(a)
+    return "/".join(a)
 
-for p in [tgtpath,libtgtspath]:
-	if not os.path.exists(p):
-		os.makedirs(p)
+
+for p in [tgtpath, libtgtspath]:
+    if not os.path.exists(p):
+        os.makedirs(p)
 
 all_names = things_in_path(inpath)
 
-def fix_file_paths(filename,curr_path_marker):
-	mypath  = os.path.abspath(".")
 
-	with open(filename,"r") as f:
-		filecontents = "".join(f)
+def fix_file_paths(filename, curr_path_marker):
+    mypath = os.path.abspath(".")
 
-	filecontents = filecontents.replace(mypath,curr_path_marker)
+    with open(filename, "r") as f:
+        filecontents = "".join(f)
 
-	with open(filename,"w") as f:
-		print(filecontents,file=f)
+    filecontents = filecontents.replace(mypath, curr_path_marker)
+
+    with open(filename, "w") as f:
+        print(filecontents, file=f)
 
 
 def run_input(name):
-	os.system("python3 "+REPL_path+" <"+inpath+"/"+name+" >"+tgtpath+"/"+name)
-	fix_file_paths(tgtpath+"/"+name,curr_path_marker)
+    os.system("python3 "+REPL_PATH+" <"+inpath+"/"+name+" >"+tgtpath+"/"+name)
+    fix_file_paths(tgtpath+"/"+name, curr_path_marker)
+
 
 def run_inputs():
-	print("making the target outputs!")
-	for n in all_names:
-		run_input(n)
+    print("making the target outputs!")
+    for n in all_names:
+        run_input(n)
 
 
-def run_broken_lib(l):
-	os.system("cp "+joinpath(libspath,l)+" "+rasplib_path)
-	os.system("python3 "+REPL_path+" <"+joinpath(libtestspath,"empty.txt")+ " >"+joinpath(libtgtspath,l))
-	
+def run_broken_lib(lib):
+    os.system("cp "+joinpath(libspath, lib)+" "+RASPLIB_PATH)
+    os.system("python3 "+REPL_PATH+" <"+joinpath(libtestspath,
+              "empty.txt") + " >"+joinpath(libtgtspath, lib))
+
 
 real_rasplib_safe_place = "make_tgts_helper/temp"
 safe_rasplib_name = "safe_rasplib.rasp"
-	
+
+
 def save_rasplib():
-	if not os.path.exists(real_rasplib_safe_place):
-		os.makedirs(real_rasplib_safe_place)
-	os.system("mv "+rasplib_path+" "+joinpath(real_rasplib_safe_place,safe_rasplib_name))
+    if not os.path.exists(real_rasplib_safe_place):
+        os.makedirs(real_rasplib_safe_place)
+    os.system("mv "+RASPLIB_PATH+" " +
+              joinpath(real_rasplib_safe_place, safe_rasplib_name))
+
 
 def restore_rasplib():
-	os.system("mv "+joinpath(real_rasplib_safe_place,safe_rasplib_name)+" "+rasplib_path)
+    os.system("mv "+joinpath(real_rasplib_safe_place,
+              safe_rasplib_name)+" "+RASPLIB_PATH)
 
 
 def run_broken_libs():
-	print("making the broken lib targets!")
-	save_rasplib()
-	all_libs = things_in_path(libspath)
-	for l in all_libs:
-		run_broken_lib(l)
-	restore_rasplib()
+    print("making the broken lib targets!")
+    save_rasplib()
+    all_libs = things_in_path(libspath)
+    for lib in all_libs:
+        run_broken_lib(lib)
+    restore_rasplib()
 
 
 if __name__ == "__main__":
-	run_inputs()
-	run_broken_libs()
+    run_inputs()
+    run_broken_libs()
